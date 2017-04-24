bba306ec7bf908a6d4707f6daa4a1a64516d8652
silvman <anoscillator@gmail.com>
2017-04-24 23:23:38 +0300

    Learning SFML graphics and key event handling

--- main.cpp
+++ main.cpp
@@ -1,22 +1,69 @@
 #include <SFML/Graphics.hpp>
 
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // Создаем главное окно приложения
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Arcanoid!");
 
-    while (window.isOpen())
+    sf::Clock clock;
+    float time;
+
+    window.setVerticalSyncEnabled(true);
+
+    sf::RectangleShape rectangle;
+    rectangle.setSize(sf::Vector2f(100, 10));
+    rectangle.setOrigin(50, 5);
+    rectangle.setPosition(400, 580);
+
+    // Главный цикл приложения
+    while(window.isOpen())
     {
+        // Обрабатываем события в цикле
         sf::Event event;
-        while (window.pollEvent(event))
+        while(window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            // Кроме обычного способа наше окно будет закрываться по нажатию на Escape
+            if(event.type == sf::Event::Closed ||
+               (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
                 window.close();
         }
 
+        // Очистка
         window.clear();
-        window.draw(shape);
+        time = clock.getElapsedTime().asMilliseconds();
+        clock.restart();
+        // Тут будут вызываться функции обновления и отрисовки объектов
+
+        const float left_border(70);
+        const float right_border(730);
+
+         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+         {
+            // left key is pressed: move our character
+            sf::Vector2f move(-time * 0.3, 0);
+            rectangle.setPosition(rectangle.getPosition() + move);
+         }
+
+         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+         {
+             // left key is pressed: move our character
+             sf::Vector2f move(time * 0.3, 0);
+             rectangle.setPosition(rectangle.getPosition() + move);
+         }
+
+         if(rectangle.getPosition().x < left_border) {
+             rectangle.setPosition(left_border, 580);
+         }
+
+
+        if(rectangle.getPosition().x > right_border) {
+            rectangle.setPosition(right_border, 580);
+        }
+
+        window.draw(rectangle);
+
+        // Отрисовка
         window.display();
     }
 
