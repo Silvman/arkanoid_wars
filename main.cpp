5b9a832ec27966b4c95fc03d377a08b95d7f6ff6
silvman <anoscillator@gmail.com>
2017-04-24 22:33:43 +0300

    linking SFML (installed in system)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
-//
-// Created by silvman on 24.04.17.
-//
+#include <SFML/Graphics.hpp>
 
-#include <iostream>
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-int main() {
-    std::cout << "My game! ^_^" << std::endl;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
