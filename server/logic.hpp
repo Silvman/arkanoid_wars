2d5915e083200460d668fa61cf673e77251791bb
alexeylive <alex_ynn1@mail.ru>
2017-05-09 19:25:01 +0300

    headers

---
+++ server/logic.hpp
@@ -0,0 +1,102 @@
+//
+// Created by alex on 09.05.17.
+//
+
+#ifndef AW_SERVER_LOGIC_HPP
+#define AW_SERVER_LOGIC_HPP
+#include "physics.h"
+
+
+class logic_world {
+private:
+
+	class logic_player {
+	private:
+		sf::Vector2f position;
+		int score, lives;
+
+	public:
+		logic_player(const float x_start, const float y_start, int lives = 3);
+		void setPosition(sf::Vector2f new_position);
+		const sf::Vector2f getPosition() const;
+		void setScore(const int new_score);
+		int getScore() const;
+		void setLives(const int new_lives);
+		int getLives() const;
+		void operator++();
+		~logic_player() {}
+	};
+
+	class logic_ball {
+	private:
+		sf::Vector2f position;
+
+	public:
+		logic_ball(const float start_x, const float start_y);
+		void setPosition(sf::Vector2f new_position);
+		const sf::Vector2f getPosition() const;
+		~logic_ball() {}
+	};
+
+	class logic_block {
+	private:
+		enum block_type {
+			simple = 0,
+			rocket,
+			slow_player,
+			fast_player,
+			freeze_enemy,
+			fast_ball
+		};
+
+		bool is_kicked;
+		int number;
+		block_type type;
+
+	public:
+		logic_block(const int number, block_type type = simple);
+		void setKicked();
+		int getNumber() const;
+		~logic_block() {}
+	};
+
+	// (1) первый игрок - снизу
+	logic_player player_bottom;
+
+	// (2) второй игрок - сверху
+	logic_player player_top;
+
+	// мячик
+	logic_ball ball;
+
+	std::vector<logic_block> blocks;
+
+	int broken_block;
+	// Флаг, содержащий информацию о том, кто потерял шарик, и (== 0) если шарик не потерян
+	int who_lost_the_ball;
+	int who_leads_the_ball;
+
+	physics_scene physics;
+
+	const float bottom_border;
+
+public:
+	logic_world(const float window_size_x,const float window_size_y);
+	void update(const unsigned int key_bottom_move, unsigned int key_bottom_action,
+			const unsigned int key_top_move, unsigned int key_top_action);
+	const sf::Vector2f givePlayerTopCoords() const ;
+	const sf::Vector2f givePlayerBottomCoords() const;
+	const sf::Vector2f giveBallCoords() const;
+	const sf::Vector2f giveBallSpeed() const;
+	int giveLivesBottom() const;
+	int giveLivesTop() const;
+	int giveScoreBottom() const;
+	int giveScoreTop() const;
+	int getBrokenBlocks();
+	bool checkPlayerKicked();
+	~logic_world() {}
+
+};
+
+
+#endif //AW_SERVER_LOGIC_HPP
---
+++ server/server.hpp
@@ -0,0 +1,72 @@
+//
+// Created by alex on 09.05.17.
+//
+
+#ifndef AW_SERVER_SERVER_HPP
+#define AW_SERVER_SERVER_HPP
+
+
+#include <SFML/Network.hpp>
+#include "logic.hpp"
+
+typedef struct data_from_server {
+	bool connection;
+	sf::Vector2f player_bottom_coords, player_top_coords, ball_coords, ball_speed;
+	int broken_block;
+	bool isPlayerKicked;
+	int score_bottom, score_top, lives_bottom, lives_top;
+} data_from_server;
+
+// эта перегрузка нужна в сервере
+sf::Packet& operator << (sf::Packet& packet, data_from_server& from);
+
+// Эта перегрузка будет нужна в клиенте
+sf::Packet& operator >> (sf::Packet& packet, data_from_server& to);
+
+struct DATA {
+	unsigned int key_bottom_move;
+	unsigned int key_bottom_action;
+	unsigned int key_top_move;
+	unsigned int key_top_action;
+	bool bot_connect;
+	bool top_connect;
+
+	unsigned num;
+};
+
+class server {
+private:
+	logic_world serv_world;
+
+	sf::TcpListener listener;
+	sf::TcpSocket socket_1; //программный интерфейс для обеспечения обмена данными между процессами
+	sf::TcpSocket socket_2;
+
+	sf::Packet INPUT;
+	sf::Packet OUTPUT;
+	bool connection;
+	//Для осуществления пакетной передачи дынных
+
+	DATA data;
+
+	data_from_server to_client;
+	/* unsigned int key_bottom_move;
+	 unsigned int key_bottom_action;
+	 unsigned int key_top_move;
+	 unsigned int key_top_action;
+
+	 unsigned num;
+ */
+
+public:
+	server(const unsigned short port);
+	~server();
+
+	void input();
+	void output();
+};
+
+
+
+
+#endif //AW_SERVER_SERVER_HPP
